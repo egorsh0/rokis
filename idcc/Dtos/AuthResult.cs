118fd801ccda7636aa7f80580e9d0a79ca5f9cdd
egorsh <egorsh@inbox.ru>
2025-05-25 13:27:17 +0300

    Документация для CompanyDto

--- idcc/Dtos/AuthResult.cs
+++ idcc/Dtos/AuthResult.cs
@@ -4,7 +4,6 @@ public class AuthResult
 {
     public bool Succeeded { get; set; }
     public List<string> Errors { get; set; } = new();
-    // Или добавлять UserId, если нужно
     public string? UserId { get; set; }
     public bool LinkedToCompany  { get; set; }
     public CompanyInfoDto? Company { get; set; }
